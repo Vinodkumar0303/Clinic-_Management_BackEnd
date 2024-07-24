0e2914610e4226cd31b7eec5b5426fdd880ebcdc
Vinodkumar0303 <23mcab38@kristujayanti.com>
2024-07-24 22:43:25 +0530

    django not working

--- backend_project/users/models.py
+++ backend_project/users/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from djongo import models as djongo_models
 
-# Create your models here.
+class MyUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('The Email field must be set')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+class MyUser(AbstractBaseUser):
+    _id = djongo_models.ObjectIdField()  # To match your MongoDB schema
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=128)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = MyUserManager()
+
+    USERNAME_FIELD = 'email'
--- backend_project/users/views.py
+++ backend_project/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth import authenticate
+from .serializers import UserSerializer
 
-# Create your views here.
+class LoginView(generics.GenericAPIView):
+    serializer_class = UserSerializer
+
+    def post(self, request, *args, **kwargs):
+        email = request.data.get('email')
+        password = request.data.get('password')
+        user = authenticate(email=email, password=password)
+        if user:
+            return Response({"message": "Login successful", "_id": str(user._id)}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_400_BAD_REQUEST)
